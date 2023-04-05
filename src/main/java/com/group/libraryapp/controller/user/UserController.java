75b93e7294c053a51ad53cddaca7d2ac5c986d26
김재령 <wofud0321@naver.com>
2023-04-05 21:26:52 +0900

    섹션2 마무리

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,31 +2,64 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "Insert into user (name, age) values(?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for(int i=0; i< users.size(); i++){
-            responses.add(new UserResponse(i+1, users.get(i)));
+        String sql = "select * from user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String readsql = "select * from user where id=?";
+        boolean isUserNotExist = jdbcTemplate.query(readsql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+        if (isUserNotExist) {// 만약 유저가 존재하지 않는다면
+            throw new IllegalArgumentException();
         }
-        return responses;
+
+        String sql = "update user set name =? where id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name){
+        String readsql = "select * from user where name=?";
+        boolean isUserNotExist = jdbcTemplate.query(readsql, (rs, rowNum) -> 0, name).isEmpty();
+        if (isUserNotExist) {// 만약 유저가 존재하지 않는다면
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "delete from user where name = ?";
+        jdbcTemplate.update(sql, name);
     }
 }
