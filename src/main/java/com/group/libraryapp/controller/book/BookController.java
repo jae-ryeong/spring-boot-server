a8bc1b805d37758223417e7737059159936c9b90
김재령 <wofud0321@naver.com>
2023-04-06 09:42:11 +0900

    BookController 생성

---
+++ src/main/java/com/group/libraryapp/controller/book/BookController.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.controller.book;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+
+
+    @PostMapping("/book")
+    public void saveBook() {
+
+    }
+}
--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,17 +1,11 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,8 +13,8 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(JdbcTemplate jdbcTemplate) {
-        userService = new UserService(jdbcTemplate);
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/user")
