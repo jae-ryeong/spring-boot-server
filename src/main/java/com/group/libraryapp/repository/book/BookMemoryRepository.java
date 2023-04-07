0ea06ec34a8ac2200b0d7c3048af0b6e0209ac8e
김재령 <wofud0321@naver.com>
2023-04-07 14:58:05 +0900

    컨테이너에게 우선권 부여

--- src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
+++ src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -1,4 +1,13 @@
 package com.group.libraryapp.repository.book;
 
-public class BookMemoryRepository {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookMemoryRepository implements BookRepository{ // 메모리에 저장하는 코드
+
+    //private final List<Book> book = new ArrayList<>();
+
+    public void saveBook() {
+        // books.add(new Book());
+    }
 }
